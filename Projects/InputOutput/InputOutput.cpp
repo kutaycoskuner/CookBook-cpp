d39e3942b3ec5a038a7a79d0aff17c9130af9762
orthrin <gronzelg@gmail.com>
2019-08-29 16:18:25 +0300

    Functions and Basics

---
+++ Projects/InputOutput/InputOutput.cpp
@@ -0,0 +1,16 @@
+//
+#include <iostream>
+
+int main()
+{
+	// Send output for directions
+	std::cout << "Enter a number\n";
+	// define a variable that we are going to use
+	int x{};
+	// get input for variable x
+	std::cin >> x;
+	// output a value of input 
+	std::cout << "You have entered" << x << "\n";
+	// retu
+	return 0
+}
