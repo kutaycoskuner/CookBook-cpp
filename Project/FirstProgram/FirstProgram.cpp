31e2cbcc669caea1e107cbcf44f8e4334a64e60f
orthrin <gronzelg@gmail.com>
2019-08-30 04:15:22 +0300

    Split Files

--- Project/FirstProgram/FirstProgram.cpp
+++ Project/FirstProgram/FirstProgram.cpp
@@ -1,51 +1,27 @@
-// InputOutput.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-// # ==== Functions ====
-void tryout_function()
-{
-	std::cout << "my function is to write this line";
-}
+// # ==== Forward Declarations ====
+void helloWorld();
+int inputOutput();
+int getValueFromUser();
 
-
+// # ==== Main ====
 int main()
 {
-	//// # ==== Input Output ====
-	////
-	//// Send output for directions
-	//std::cout << "Enter a number\n";
-	//// define a variable that we are going to use
-	//int x{};
-	//// get input for variable x
-	//std::cin >> x;
-	//// output a value of input 
-	//std::cout << "You have entered " << x << "\n";
-	//// return
-	//return 0;
-
-	//// # ==== First Program: Multiple and Triple Given Value ====
-	////
-	//std::cout << "Enter an integer" << "\n";
-	//int input_num{0};
-	//std::cin >> input_num;
-	//std::cout << "Double of " << input_num << " is " << input_num  * 2 << "\n";
-	//std::cout << "Triple of " << input_num << " is "<< input_num * 3 << "\n";
-	//return 0;
-
-	// # ==== Function Test ====
-	tryout_function();
-	tryout_function();
+	helloWorld();
+	inputOutput();
+	std::cout << "double of your number is " << getValueFromUser() * 2 << '\n';
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+	// # ==== Function Test ====
+	//// - Require tryout_function()
+	//tryout_function();
+
+	// # ==== Function Return Values ====
+	// - require getValueFromUser()
+	/*int num{ getValueFromUser() };
+
+	std::cout << num << " double is : " << num * 2 << '\n';
+	return 0;*/
---
+++ Project/FirstProgram/inputOutput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int inputOutput()
+{
+	// # ==== Input Output ====
+	//
+	// Send output for directions
+	std::cout << "Enter a number" << '\n';
+	// define a variable that we are going to use
+	int x{};
+	// get input for variable x
+	std::cin >> x;
+	// output a value of input 
+	std::cout << "You have entered " << x << "\n";
+	// return
+	return 0;
+}
