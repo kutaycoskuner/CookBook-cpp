1e5ba8240e77ed6f1ad5e582b5e41f2942cdb8f2
orthrin <gronzelg@gmail.com>
2019-09-10 02:13:38 +0300

    6-9a

--- Project/FirstProgram/FirstProgram.cpp
+++ Project/FirstProgram/FirstProgram.cpp
@@ -17,7 +17,32 @@
 
 // # ==== Question ====
 //
-// Prime Finder
+// // # ==== Question ====
+//
+//Write a program that :
+//*Asks the user how many names they wish to enter.
+//* Asks the user to enter each name.
+//* Calls a function to sort the names(modify the selection sort code from lesson 6.4 --Sorting an array using selection sort)
+//* Prints the sorted list of names.
+//
+// Hint : Use a dynamic array of std::string to hold the names.
+//	Hint : std::string supports comparing strings via the comparison operators <and >
+//
+//	Your output should match this :
+//
+//	How many names would you like to enter ? 5
+//	Enter name #1: Jason
+//	Enter name #2: Mark
+//	Enter name #3: Alex
+//	Enter name #4: Chris
+//	Enter name #5: John
+//
+//	Here is your sorted list :
+//Name #1: Alex
+//Name #2: Chris
+//Name #3: Jason
+//Name #4: John
+//Name #5: Mark
 
 // # ==== Namespaces ====
 //
@@ -30,41 +55,49 @@
 
 // # ==== Temporary Functions ====
 //
+void sortArray(std::string *array, int length)
+{
+	// ==== Sorting array ====
+	for (int jjj = 0; jjj < length-1;++jjj)
+	{
+		// Testing if value greater than static j while i is changing
+		for (int iii = jjj+1; iii < length; ++iii)
+		{
+			// if it is greater swapping those two elements
+			if (array[jjj] > array[iii])
+			{
+				std::swap(array[jjj], array[iii]);
+			}
+		}
+	}
+}
 
 // # ==== Main ====
 //
 int main()
 {
 	// Start Main
-	const int whichPrime = 1000; // Kacinci Prime Lazim?
-	bool primeTest{ true };
-	long primeArray[whichPrime] = {2,3,5,7};
-	long num = 10;
+	std::cout << "How many names would you like to enter? "; // ttt
+	int length;
+	std::cin >> length;
 
-	for (int iii = 4; iii < whichPrime; ++num)
+	std::string *nameList = new std::string[length]; // use array new.  Note that length does not need to be constant!
+
+	for (int iii = 0; iii < length; ++iii)
 	{
-		primeTest = true;
-
-		for (int jjj = 2; jjj < num/2; ++jjj)
-		{
-			if (num % jjj == 0)
-			{
-				primeTest = false;
-				break;
-			}
-		}
-
-		if (primeTest)
-		{
-			primeArray[iii] = num;
-			++iii;
-		}
+		std::cout << "Enter name #" << iii+1 << "\n";
+		std::cin >> nameList[iii];
 	}
 
-	for (int iii = 0; iii < whichPrime; ++iii)
-	{
-		std::cout << iii+1 << "- " << primeArray[iii] << "\n";
-	}
+	sortArray(nameList, length);
+
+	for (int iii = 0; iii < length; ++iii)
+		std::cout << nameList[iii] << "\n";
+
+	delete[] nameList;
+
+
+
 	// Return
 	return 0;
 }
--- Project/FirstProgram/old_Main.cpp
+++ Project/FirstProgram/old_Main.cpp
@@ -1,3 +1,91 @@
+//// ==== 6-9a Dynamic Memory Allocation and Sorting ====
+//// # ==== Question ====
+////
+//// // # ==== Question ====
+////
+////Write a program that :
+////*Asks the user how many names they wish to enter.
+////* Asks the user to enter each name.
+////* Calls a function to sort the names(modify the selection sort code from lesson 6.4 --Sorting an array using selection sort)
+////* Prints the sorted list of names.
+////
+//// Hint : Use a dynamic array of std::string to hold the names.
+////	Hint : std::string supports comparing strings via the comparison operators <and >
+////
+////	Your output should match this :
+////
+////	How many names would you like to enter ? 5
+////	Enter name #1: Jason
+////	Enter name #2: Mark
+////	Enter name #3: Alex
+////	Enter name #4: Chris
+////	Enter name #5: John
+////
+////	Here is your sorted list :
+////Name #1: Alex
+////Name #2: Chris
+////Name #3: Jason
+////Name #4: John
+////Name #5: Mark
+//
+//// # ==== Namespaces ====
+////
+//
+//// # ==== Enumerators ====
+////
+//
+//// # ==== Structs ====
+////
+//
+//// # ==== Temporary Functions ====
+////
+//void sortArray(std::string* array, int length)
+//{
+//	// ==== Sorting array ====
+//	for (int jjj = 0; jjj < length - 1;++jjj)
+//	{
+//		// Testing if value greater than static j while i is changing
+//		for (int iii = jjj + 1; iii < length; ++iii)
+//		{
+//			// if it is greater swapping those two elements
+//			if (array[jjj] > array[iii])
+//			{
+//				std::swap(array[jjj], array[iii]);
+//			}
+//		}
+//	}
+//}
+//
+//// # ==== Main ====
+////
+//int main()
+//{
+//	// Start Main
+//	std::cout << "How many names would you like to enter? "; // ttt
+//	int length;
+//	std::cin >> length;
+//
+//	std::string* nameList = new std::string[length]; // use array new.  Note that length does not need to be constant!
+//
+//	for (int iii = 0; iii < length; ++iii)
+//	{
+//		std::cout << "Enter name #" << iii + 1 << "\n";
+//		std::cin >> nameList[iii];
+//	}
+//
+//	sortArray(nameList, length);
+//
+//	for (int iii = 0; iii < length; ++iii)
+//		std::cout << nameList[iii] << "\n";
+//
+//	delete[] nameList;
+//
+//
+//
+//	// Return
+//	return 0;
+//}
+
 //// # ==== Orth: Prime Finder ====
 //// # ==== Question ====
 ////
