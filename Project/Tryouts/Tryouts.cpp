7a950562d139b781d3541734dcfd6a33d29d1bbb
orthrin <gronzelg@gmail.com>
2019-09-11 03:46:26 +0300

    New Initialization for Chapter 7

---
+++ Project/Tryouts/Tryouts.cpp
@@ -0,0 +1,27 @@
+// ==== Precursor Directives
+#include <iostream>
+#include <string>
+#include <array>
+#include <random>				// for std::mt19937
+#include <utility>				// for std::sort
+#include <ctime>				// for std::time
+#include <algorithm>
+
+// ==== Question
+
+// ==== Namespace
+
+// ==== Enum
+
+// ==== Struct
+
+// ==== Functions
+
+int main()
+{
+	// ==== Main
+    std::cout << "Hello World!\n";
+
+	// ==== Return
+	return 0;
+}
