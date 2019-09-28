cdbde3adfa9c11c794005bf754b74a635ecef718
orthrin <gronzelg@gmail.com>
2019-09-29 00:23:54 +0300

    organization, 91-start

---
+++ Project/Tutorials/Tutorials.cpp
@@ -0,0 +1,18 @@
+// ==== Precursor Directives
+#include <iostream>
+#include <string>
+#include <array>
+#include <random>				// for std::mt19937
+#include <utility>				// for std::sort
+#include <ctime>				// for std::time
+#include <cassert>				// for assert
+#include <cstdlib>				// for rand() and srand()
+#include <algorithm>
+
+// ==== Class
+
+int main()
+{
+
+	return 0;
+}
